282e212330d88f26da51ac66f41d307cb15fbf20
Blackmemo <Apst12012@gmail.com>
2019-04-02 16:41:08 -0400

    completed mvp and stretch

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-
 import SavedList from './Movies/SavedList';
 import MovieList from './Movies/MovieList';
 import Movie from './Movies/Movie';
+import { Route } from 'react-router-dom'
 
 export default class App extends Component {
   constructor() {
@@ -16,13 +16,15 @@ export default class App extends Component {
     const savedList = this.state.savedList;
     savedList.push(movie);
     this.setState({ savedList });
-  };
+    }
+  
 
   render() {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Route exact path="/" component={MovieList} />
+        <Route path="/movies/:id" render={(props) => <Movie {...props} addToSavedList={this.addToSavedList} />} />
       </div>
     );
   }
